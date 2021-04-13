12bca67c15770553c1e3568db3348b8b67af5be8
jrlr5 <jrlr5@LAPTOP-BMP93VK5>
2021-04-12 21:25:53 -0500

    practica 2 a 5

---
+++ NetBeansProjects/G4S21jrlrp2verdadera/src/g4s21jrlrp2verdadera/G4S21jrlrp2verdadera.java
@@ -0,0 +1,17 @@
+
+package g4s21jrlrp2verdadera;
+
+/**
+ *
+ * @author jrlr5
+ */
+public class G4S21jrlrp2verdadera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FrmPrincipal().setVisible(true);
+    }
+    
+}
